ef5bd71bf5f55a6b0cedbb1df614c6a69beb4a69
Mustafa Yatagan <mustafayatagan@gmail.com>
2023-01-17 14:01:28 +0300

    orbit Contorls Added

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
@@ -14,6 +15,8 @@ camera.position.z = 2
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
